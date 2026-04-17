cabf90075e58d3756700eb7aecf1753b8e8b1ca6
edielynsilvano-sys <edielynsilvano@gmail.com>
2026-04-17 23:09:42 +0800

    Activity 2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,33 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+
   protected readonly title = signal('Integrative-Programming-and-Technologies-2');
+
+  page: 'login' | 'profile' = 'login';
+
+  username = '';
+  password = '';
+
+  login() {
+    if (this.username && this.password) {
+      this.page = 'profile';
+    } else {
+      alert('Please enter username and password');
+    }
+  }
+
+  logout() {
+    this.page = 'login';
+    this.username = '';
+    this.password = '';
+  }
 }
